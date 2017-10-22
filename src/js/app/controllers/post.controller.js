7432083519de0e7f42d367bc52058cc1b18e5774
Tsvetomir Dochev <tddochev@gmail.com>
2017-10-22 14:35:25 +0300

    working on creating post

--- src/js/app/controllers/post.controller.js
+++ src/js/app/controllers/post.controller.js
@@ -4,12 +4,29 @@ export default class postController {
         this._data = data;
         this._template = template;
         this._categories = [];
+        this.context;
     };
 
     add(param) {
         console.log('in post add' + param);
         this._template.loadTemplate('test').then((compiledTemplate) => {
-            $('#app-main').html(compiledTemplate(this._categories));
-        })
+            this._data.getCategories().then(categories =>{
+                this.context = {
+                    categories: categories
+                }
+                console.log(this.context.categories);
+                $('#app-main').html(compiledTemplate(this.context));
+            });
+
+            //Add Category
+            $('#addCategoryBtn').on('click', ()=> {
+                const categoryToAdd = $('#addCategory')[0].value;
+                if(categoryToAdd){
+                    $('#postCategory').append(`<option>${categoryToAdd}</option>`);
+                    this._data.addCategorie(categoryToAdd);
+                    console.log('Add category' + categoryToAdd);
+                }
+            });
+        });
     }
 }
