41c67e8a398f05c0ccbbd9d75fc85db31c9a455f
Tsvetomir Dochev <tddochev@gmail.com>
2017-10-22 15:47:42 +0300

    working on post controller

--- src/js/app/controllers/post.controller.js
+++ src/js/app/controllers/post.controller.js
@@ -1,3 +1,5 @@
+import Post from './../../models/post.model';
+
 export default class postController {
 
     constructor(data, template) {
@@ -10,23 +12,43 @@ export default class postController {
     add(param) {
         console.log('in post add' + param);
         this._template.loadTemplate('test').then((compiledTemplate) => {
-            this._data.getCategories().then(categories =>{
+            this._data.getCategories().then(categories => {
                 this.context = {
                     categories: categories
                 }
                 console.log(this.context.categories);
                 $('#app-main').html(compiledTemplate(this.context));
-            });
 
-            //Add Category
-            $('#addCategoryBtn').on('click', ()=> {
-                const categoryToAdd = $('#addCategory')[0].value;
-                if(categoryToAdd){
-                    $('#postCategory').append(`<option>${categoryToAdd}</option>`);
-                    this._data.addCategorie(categoryToAdd);
-                    console.log('Add category' + categoryToAdd);
-                }
+                //add category
+                $('#addCategoryBtn').on('click', () => {
+                    const categoryToAdd = $('#addCategory')[0].value;
+                    if (categoryToAdd) {
+                        $('#postCategory').append(`<option>${categoryToAdd}</option>`);
+                        this._data.addCategorie(categoryToAdd);
+                        console.log('Add category' + categoryToAdd);
+                    }
+                });
+
+                //add post
+                $('#submitPost').on('click', () => {
+                    const title = $('#postTitle')[0].value;
+                    const contetnt= $('#postCongtent')[0].value;
+                    const category= $('#postCategory')[0].value
+                    const author= $('#postAuthor')[0].value;
+                    const date =  moment().format();
+                    const imageUrl = 'https://lorempixel.com/222/180/';                 
+
+                    const post = new Post(title, contetnt, category ,author,date,imageUrl);
+                    this._data.addPost(post);
+
+                });
             });
         });
+    }
+
+    show(id) {
+        this._data.getPostByID(id).then(post => {
+            console.log(post);
+        })
     }
 }
--- src/js/app/modules/data.js
+++ src/js/app/modules/data.js
@@ -17,4 +17,17 @@ export default class Data {
             });
         });
     }
+    addPost(post) {
+        var newPostKey = this._db.ref().child('posts').push().key;
+        var updates = {};
+        updates['/posts/' + newPostKey] = post;
+        return firebase.database().ref().update(updates);
+    }
+    getPostByID(id) {
+        return new Promise((resolve, reject) => {
+            this._db.ref('/posts/' + id).once('value').then((snapshot) => {
+                resolve(snapshot.val())
+            });
+        });;
+    }
 }
